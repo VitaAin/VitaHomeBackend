a283ed6705949a34c7e157f32e8e4d28c7e03aea
VitaAin <wtaincb@126.com>
2017-11-14 18:21:44 +0800

    create article, categories, and tags table

---
+++ database/migrations/2017_11_14_085320_create_articles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("title")->comment("文章标题");
+            $table->text("body")->comment("文章内容"); 
+            $table->dateTime("post_time")->comment("文章发布时间");
+            $table->integer("user_id")->unsigned()->comment("作者id");
+            $table->integer("last_comment_user_id")->unsigned()->default(0)->index()->comment("最后评论用户id");
+            $table->integer("category_id")->unsigned()->default(0)->index()->comment("所属类别");
+            $table->integer("view_count")->unsigned()->default(0)->index()->comment("查看此次数");
+            $table->integer("comments_count")->unsigned()->default(0)->comment("评论数");
+            $table->integer("likes_count")->unsigned()->default(0)->comment("点赞数");
+            $table->enum("close_comment", [0, 1])->default(0)->index()->comment("是否关闭评论");
+            $table->enum("is_public", [0, 1])->default(1)->index()->comment("是否公开");
+            $table->enum("is_top", [0, 1])->default(0)->index()->comment("是否置顶");
+            $table->enum("is_excellent", [0, 1])->default(0)->index()->comment("是否为精华");
+            $table->timestamp('last_comment_time')->comment('最后评论时间');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
---
+++ database/migrations/2017_11_14_085508_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name")->comment("类别名称");
+            $table->string("description")->comment("类别描述");
+            $table->integer("articles_count")->comment("文章数量");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
