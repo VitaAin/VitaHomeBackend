4f943acbc51cf9fb6331a9be1a913879c697be77
VitaAin <wtaincb@126.com>
2017-12-08 10:25:56 +0800

    correct url of register-verify

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
     {
         $bind_data = [
             'name' => $user->name,
-            'verify_url' => 'http://vitain.top/verify_email/?code=' . $user->confirm_code
+            'verify_url' => 'http://admin.vitain.top/verify_email/?code=' . $user->confirm_code
         ];
         $template = new SendCloudTemplate('vitahome_register_verify', $bind_data);
 
