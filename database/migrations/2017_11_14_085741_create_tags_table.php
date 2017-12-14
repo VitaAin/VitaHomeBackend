13f47f761bd0d408930cd1e1c755672645a79bb7
VitaAin <wtaincb@126.com>
2017-12-14 13:09:35 +0800

    add default value

--- database/migrations/2017_11_14_085741_create_tags_table.php
+++ database/migrations/2017_11_14_085741_create_tags_table.php
@@ -16,7 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string("name")->comment("类别名称");
-            $table->string("description")->comment("类别描述");
+            $table->string("description")->default("")->comment("类别描述");
             $table->integer("articles_count")->comment("文章数量");
             $table->timestamps();
         });
