3c8bbce57773f7958c07c6fb5407d6004276402a
VitaAin <wtaincb@126.com>
2017-12-18 00:25:29 +0800

    do a test

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -89,6 +89,13 @@ class ArticlesController extends Controller
             'category_id' => $category
         ];
         $article = $this->articlesRepository->create($data);
+        $images = $request->get('images');
+        foreach ($images as $image) {
+//            if ($image->article_id) continue;
+            echo '----------------------------';
+            echo $image->name;
+            $image->article_id = $article->id;
+        }
         $images = $this->articlesRepository->createImages($article->id, $request->get('images'));
         Auth::user()->increment('articles_count');
         Auth::user()->increment('images_count', count($images));
