bb97badc003dce57078b937dafe037f623e1cc44
VitaAin <wtaincb@126.com>
2017-12-19 01:12:42 +0800

    delete

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -95,7 +95,6 @@ class ArticlesController extends Controller
         Auth::user()->increment('images_count', count($images));
         $article->increment('images_count', count($images));
         $article->tags()->attach($tags);
-//        $article->images()->attach($images);
         Cache::tags('articles')->flush();
 
         return $this->responseOk('OK', $article);
