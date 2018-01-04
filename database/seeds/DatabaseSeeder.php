dc8ef25f17be80923f46c4603672d0375b633d13
VitaAin <wtaincb@126.com>
2018-01-04 16:45:48 +0800

    remove PermissionRoleTableSeeder due to not work

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(MenusTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
     }
 }
