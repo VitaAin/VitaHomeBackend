54830e185a6d741d8c9b1fc44847da90b2830c58
VitaAin <wtaincb@126.com>
2018-01-05 16:15:16 +0800

    modify casts in User

--- app/User.php
+++ app/User.php
@@ -36,7 +36,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $casts = [
-        'is_banned' => 'boolean',
+//        'is_banned' => 'boolean',
 //        'is_confirmed' => 'boolean'
     ];
 
