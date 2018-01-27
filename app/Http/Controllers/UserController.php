88955019d3ab13a8611939f344c182468d883b38
VitaAin <wtaincb@126.com>
2018-01-28 00:52:33 +0800

    modify

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -152,7 +152,8 @@ class UserController extends Controller
         if ($clientOriginalExt && !in_array($clientOriginalExt, $allowed_extensions)) {
             return $this->responseError('You can only upload png, jpg/jpeg or gif.');
         }
-        $fileName = md5(time()) . '.' . $clientOriginalExt;
+        $md5 = md5(time());
+        $fileName = $md5 . '.' . $clientOriginalExt;
         $fileMoved = $file->move(public_path('../storage/app/public/user_images/' . Auth::id()), $fileName);
 
         // 要访问下面这个url（storage中的文件资源），需要为storage目录建立软连接到public/storage，执行：php artisan storage:link
@@ -165,8 +166,8 @@ class UserController extends Controller
                 ->fit(300, 200)->save();
             $data = [
                 "user_id" => Auth::id(),
-                "uid" => $fileName,
-                "name" => $file->getFilename(),
+                "uid" => $md5,
+                "name" => $file->getBasename(),
                 "url" => $imageUrl,
                 "size" => $fileMoved->getSize()
             ];
